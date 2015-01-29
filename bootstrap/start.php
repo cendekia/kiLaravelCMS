1861b2d23b7f587ea6c9f04b38a900c098ac67ca
cendekiapp <cendiqkrn@gmail.com>
2015-01-30 05:29:07 +0700

    set up local and production environment on db configuration

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,14 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
+// $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead'),
+// 	'local' => array('homestead'),
 
-));
+// ));
+$env = $app->detectEnvironment(function(){
+    return getenv('APP_ENV') ?: 'local'; 
+});
 
 /*
 |--------------------------------------------------------------------------
