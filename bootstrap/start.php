86e2df7728089d056e1459029528fb29bccc5901
cendekiapp <cendiqkrn@gmail.com>
2015-01-30 13:14:19 +0700

    change detectEnvironment method

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,13 +24,8 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-// $env = $app->detectEnvironment(array(
-
-// 	'local' => array('homestead'),
-
-// ));
 $env = $app->detectEnvironment(function(){
-    return getenv('APP_ENV') ?: 'local'; 
+    return getenv('APP_ENV')?:'local'; 
 });
 
 /*
